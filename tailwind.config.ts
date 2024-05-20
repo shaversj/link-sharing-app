390b6804c4310aa87985706ff8718122c46371ef
Josh Shavers <shaversj@gmail.com>
2024-05-20 08:28:56 -0500

    Project Setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,28 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
+    screens: {
+      sm: "375px",
+      lg: "1440px",
+    },
     extend: {
+      colors: {
+        purple: "#633CFF",
+        "purple-light-hover": "#BEADFF",
+        "purple-light": "#EFEBFF",
+        "gray-dark": "#737373",
+        "gray-medium": "#D9D9D9",
+        "gray-light": "#FAFAFA",
+        red: "#FF3939",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        instrumentSans: ["var(--font-instrumentSans)"],
       },
     },
   },
