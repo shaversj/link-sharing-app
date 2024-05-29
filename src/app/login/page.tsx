18b06d77d3d45a6b8b0cb959c6dfbf54ea2c352e
Josh Shavers <shaversj@gmail.com>
2024-05-29 10:57:25 -0500

    Update Input component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,17 +3,17 @@ import Input from "@/components/Input";
 
 export default function LoginPage() {
   return (
-    <div className={"font-instrumentSans w-[375px] border p-8 antialiased"}>
+    <div className={"w-[375px] border p-8 font-instrumentSans antialiased"}>
       <Image src="/images/logo-devlinks-large.svg" alt="Logo" width={182.5} height={40} />
       <div className={"space-y-[8px] pt-[64px]"}>
-        <h2 className={"text-dark-gray text-left align-top text-[24px] font-bold leading-[150%]"}>Login</h2>
-        <p className={"text-gray text-[15.9px] leading-[150%]"}>Add your details below to get back into the app</p>
+        <h2 className={"text-left align-top text-[24px] font-bold leading-[150%] text-dark-gray"}>Login</h2>
+        <p className={"text-[15.9px] leading-[150%] text-gray"}>Add your details below to get back into the app</p>
       </div>
 
       <form className={"pt-[40px]"}>
         <div className={"space-y-[24px]"}>
-          <Input label="Email address" iconType="email" type="email" name="email" placeholder="e.g. alex@email.com" />
-          <Input label="Password" iconType="password" type="password" name="password" placeholder="Enter your password" />
+          <Input label="Email address" iconVariant="email" type="email" name="email" placeholder="e.g. alex@email.com" />
+          <Input label="Password" iconVariant="password" type="password" name="password" placeholder="Enter your password" />
         </div>
 
         <button type="submit">Login</button>
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,35 +1,24 @@
-import { cn } from "@/lib/utils";
-import { cva, type VariantProps } from "class-variance-authority";
-
 export type InputProps = {
   label: string;
-  iconType: "default" | "email" | "password";
+  iconVariant: "email" | "password";
   type: string;
   name: string;
   placeholder: string;
 };
 
-export default function Input({ label, iconType, type, name, placeholder }: InputProps) {
-  const inputVariants = cva(
-    "text-dark-gray block w-full rounded-lg border border-gray-medium bg-left bg-no-repeat bg-origin-content px-4 py-3 text-[16px] leading-none placeholder:px-7 placeholder:leading-[150%]",
-    {
-      variants: {
-        iconType: {
-          default: "",
-          email: "bg-[url('/images/icon-email.svg')]",
-          password: "bg-[url('/images/icon-password.svg')]",
-        },
-      },
-      defaultVariants: {
-        iconType: "default",
-      },
-    },
-  );
+export default function Input({ label, iconVariant, type, name, placeholder }: InputProps) {
+  const iconVariants = {
+    email: "bg-[url('/images/icon-email.svg')]",
+    password: "bg-[url('/images/icon-password.svg')]",
+  };
+
+  const baseClasses =
+    "text-dark-gray block w-full rounded-lg border border-gray-medium bg-left bg-no-repeat bg-origin-content px-4 py-3 text-[16px] leading-none placeholder:px-7 placeholder:leading-[150%]";
 
   return (
     <div>
-      <label className={"text-dark-gray text-left align-top text-[12px] leading-[150%]"}>{label}</label>
-      <input className={cn(inputVariants({ iconType }))} type={type} name={name} placeholder={placeholder} />
+      <label className={"text-left align-top text-[12px] leading-[150%] text-dark-gray"}>{label}</label>
+      <input className={`${baseClasses} ${iconVariants[iconVariant]}`} type={type} name={name} placeholder={placeholder} />
     </div>
   );
 }
