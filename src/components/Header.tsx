37719b8b811a22fb49af5312ca80a0da11f97273
Josh Shavers <shaversj@gmail.com>
2024-06-30 10:19:22 -0500

    Update Nav Bar formatting

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,24 +5,25 @@ export default function Header({ activePage }: { activePage: string }) {
   return (
     <>
       <div className={"py-4 pl-6 pr-4"}>
-        <nav className={"flex justify-center"}>
-          <div className={"flex min-h-full w-[3.25rem] items-center justify-start"}>
+        <nav className={"flex items-center"}>
+          <div className={"mr-auto"}>
             <Image src="/images/logo-devlinks-small.svg" alt="logo" width={32} height={32} />
           </div>
+
           <Link href={"/links"}>
-            <div className={`ml-auto flex items-center justify-center rounded-md px-[1.688rem] py-[0.688rem] ${activePage === "links" ? "bg-purple-light" : ""}`}>
+            <div className={`grid h-[42px] w-[74px] place-items-center ${activePage === "links" ? "rounded-md bg-purple-light" : ""}`}>
               <Image src="/images/icon-links-header.svg" alt="logo" width={21} height={20} />
             </div>
           </Link>
 
           <Link href={"/details"}>
-            <div className={`flex items-center justify-center px-[1.688rem] ${activePage === "details" ? "bg-purple-light" : ""}`}>
+            <div className={`grid h-[42px] w-[74px] place-items-center ${activePage === "details" ? "rounded-md bg-purple-light" : ""}`}>
               <Image src="/images/icon-profile-details-header.svg" alt="logo" width={21} height={20} />
             </div>
           </Link>
 
-          <Link href={"/profile"}>
-            <div className={`flex items-center justify-center px-[1.688rem] ${activePage === "profile" ? "bg-purple-light" : ""}`}>
+          <Link href={"/profile"} className={"ml-auto"}>
+            <div className={`grid h-[42px] w-[52px] place-items-center rounded-md border border-purple ${activePage === "profile" ? "bg-purple-light" : ""}`}>
               <Image src="/images/icon-preview-header.svg" alt="logo" width={20} height={20} />
             </div>
           </Link>
