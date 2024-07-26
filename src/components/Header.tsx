ac7714c80be857bd564ed69e407751163f389e47
Josh Shavers <shaversj@gmail.com>
2024-07-26 07:32:36 -0500

    Add Tablet layout to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -35,24 +35,36 @@ export default function Header({ activePage, userId }: { activePage: string; use
           <div className={"bg-white py-4 pl-6 pr-4"}>
             <nav className={"flex items-center"}>
               <div className={"mr-auto"}>
-                <Image src="/images/logo-devlinks-small.svg" alt="logo" width={32} height={32} />
+                <Image className={"md:hidden"} src="/images/logo-devlinks-small.svg" alt="logo" width={32} height={32} />
+                <Image className={"md:block hidden"} src="/images/logo-devlinks-large.svg" alt="logo" width={146} height={32} />
               </div>
 
               <Link href={"/links"}>
-                <div className={`grid h-[42px] w-[74px] place-items-center ${activePage === "links" ? "rounded-md bg-purple-light" : ""}`}>
-                  <Image src="/images/icon-links-header.svg" alt="logo" width={21} height={20} />
+                <div
+                  className={`md:w-[7.625rem] md:h-[2.875rem] grid h-[2.625rem] w-[4.625rem] place-items-center ${activePage === "links" ? "rounded-md bg-purple-light text-purple" : "text-project-gray"}`}
+                >
+                  <div className={"md:flex md:gap-x-2"}>
+                    <Image src="/images/icon-links-header.svg" alt="logo" width={21} height={20} />
+                    <h3 className={"md:block hidden font-instrumentSans font-semibold"}>Links</h3>
+                  </div>
                 </div>
               </Link>
 
               <Link href={"/profile"}>
-                <div className={`grid h-[42px] w-[74px] place-items-center ${activePage === "profile" ? "rounded-md bg-purple-light" : ""}`}>
-                  <Image src="/images/icon-profile-details-header.svg" alt="logo" width={21} height={20} />
+                <div
+                  className={`md:w-[11.688rem] md:h-[3rem] grid h-[2.625rem] w-[4.625rem] place-items-center ${activePage === "profile" ? "rounded-md bg-purple-light text-purple" : "text-project-gray"}`}
+                >
+                  <div className={"md:flex md:gap-x-2"}>
+                    <Image src="/images/icon-profile-details-header.svg" alt="logo" width={21} height={20} />
+                    <h3 className={"md:block hidden font-instrumentSans font-semibold"}>Profile Details</h3>
+                  </div>
                 </div>
               </Link>
 
               <Link href={`/profile/${userId}`} className={"ml-auto"}>
-                <div className={`grid h-[42px] w-[52px] place-items-center rounded-md border border-purple`}>
-                  <Image src="/images/icon-preview-header.svg" alt="logo" width={20} height={20} />
+                <div className={`md:w-[7.125rem] md:h-[2.875rem] grid h-[2.625rem] w-[3.25rem] place-items-center rounded-md border border-purple`}>
+                  <Image className={"md:hidden"} src="/images/icon-preview-header.svg" alt="logo" width={20} height={20} />
+                  <h3 className={"md:block hidden font-instrumentSans font-semibold text-purple"}>Preview</h3>
                 </div>
               </Link>
             </nav>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,7 @@ const config: Config = {
         "purple-light-hover": "#BEADFF",
         "purple-light": "#EFEBFF",
         gray: "#737373",
+        "project-gray": "#737373",
         "dark-gray": "#333333",
         "gray-medium": "#D9D9D9",
         "gray-light": "#FAFAFA",
