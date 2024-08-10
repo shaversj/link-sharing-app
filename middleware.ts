469c523922229d8ad80f4b6b9bec8b4f5c8d1157
Josh Shavers <shaversj@gmail.com>
2024-08-10 15:04:44 -0500

    Update auth middleware

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,13 @@
-export { auth as middleware } from "./auth";
+import { auth } from "./auth";
+
+// export { auth as middleware } from "./auth";
+
+export default auth((req) => {
+  if (!req.auth && req.nextUrl.pathname == "/api/") {
+    return Response.json({ message: "Not authenticated" }, { status: 401 });
+  }
+
+  if (!req.auth && req.nextUrl.pathname !== "/login") {
+    return Response.redirect(`${process.env.NEXT_PUBLIC_BACKEND_URL}error?error=NotAuthenticated`, 302);
+  }
+});
--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,10 +1,9 @@
 import { getUser } from "@/components/actions";
 import { NextRequest, NextResponse } from "next/server";
-import { auth } from "../../../../auth";
 
 export async function GET(request: NextRequest) {
-  const session = await auth();
-  if (!session?.user) return NextResponse.json({ message: "Not authenticated" }, { status: 401 });
+  // const session = await auth();
+  // if (!session?.user) return NextResponse.json({ message: "Not authenticated" }, { status: 401 });
 
   const userId = request.nextUrl.searchParams.get("userId");
   return NextResponse.json(await getUser(userId || ""));
