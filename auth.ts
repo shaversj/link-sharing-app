a7737191b0f5aff5ca6619e803e12d48e05e55dc
Josh Shavers <shaversj@gmail.com>
2024-08-10 12:53:19 -0500

    Update Github config

--- auth.ts
+++ auth.ts
@@ -41,6 +41,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: DrizzleAdapter(db),
   providers: [
     GitHub({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
       allowDangerousEmailAccountLinking: true,
     }),
     Credentials({
